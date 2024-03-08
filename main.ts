d5f57ddd87e64486aa1a1eea376a9e00a3c6a3fe
Tsisar <fhantom.tp@gmail.com>
2024-03-09 00:15:28 +0100

    remove

--- main.ts
+++ main.ts
@@ -32,15 +32,15 @@ function doMatrix () {
     }
 }
 input.onGesture(Gesture.LogoUp, function () {
-    reset()
+    repeat = 24
     logo = 0
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    reset()
+    repeat = 24
     logo = 3
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    reset()
+    repeat = 24
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (pause2 < 1000) {
@@ -55,43 +55,64 @@ function plotDown (y: number) {
     led.plot(x, 4 - y)
     led.unplot(x, 5 - y)
 }
+function showHeart () {
+    heart = false
+    basic.showIcon(IconNames.Heart, 600)
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.SmallHeart, pause2)
+        basic.showIcon(IconNames.Heart, pause2)
+    }
+    basic.clearScreen()
+}
 function plotUp (y: number) {
     led.plot(x, y)
     led.unplot(x, y - 1)
 }
 input.onGesture(Gesture.TiltRight, function () {
-    reset()
+    repeat = 24
     logo = 2
 })
 input.onGesture(Gesture.LogoDown, function () {
-    reset()
+    repeat = 24
     logo = 1
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    reset()
-    basic.showIcon(IconNames.Heart, pause2)
-    basic.showIcon(IconNames.SmallHeart, pause2)
-    basic.showIcon(IconNames.Heart, pause2)
+    repeat = 24
+    heart = true
 })
 function reset () {
     repeat = 0
     for (let index = 0; index <= 4; index++) {
         list[index] = 5
     }
+    remove()
     basic.clearScreen()
+}
+function remove () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.unplot(x, y)
+        }
+        basic.pause(pause2)
+    }
 }
 let max = 0
 let list: number[] = []
 let x = 0
 let y = 0
+let heart = false
 let repeat = 0
 let logo = 0
 let pause2 = 0
 pause2 = 100
 logo = 0
 repeat = 0
+heart = false
 basic.forever(function () {
     reset()
+    if (heart) {
+        showHeart()
+    }
     while (repeat < 25) {
         repeat += 1
         doMatrix()
